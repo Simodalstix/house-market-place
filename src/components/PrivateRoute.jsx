f2843d97886dd9780cc4f750458736d21352ded2
Simodalstix <lastpolar@gmail.com>
2022-04-06 11:24:02 +1000

    Commandline, need more practise

---
+++ src/components/PrivateRoute.jsx
@@ -0,0 +1,10 @@
+import { Navigate, Outlet } from 'react-router-dom';
+import { useAuthStatus } from '../hooks/useAuthStatus';
+
+
+const PrivateRoute = () => {
+  const loggedIn = false;
+
+  return loggedIn ? <Outlet /> : <Navigate to='/sign-in' />;
+};
+export default PrivateRoute;
---
+++ src/firebase.config.js
@@ -0,0 +1,23 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from 'firebase/app';
+import { getAnalytics } from 'firebase/analytics';
+import { getFirestore } from 'firebase/firestore';
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: 'AIzaSyBWFqAlMJJyT-yGRXKYg1ZvBbtjGMekTtc',
+  authDomain: 'house-marketplace-app-8cdf2.firebaseapp.com',
+  projectId: 'house-marketplace-app-8cdf2',
+  storageBucket: 'house-marketplace-app-8cdf2.appspot.com',
+  messagingSenderId: '697883983072',
+  appId: '1:697883983072:web:5f1d801c1266889606a485',
+  measurementId: 'G-5FWX3EWTBR',
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
+export const db = getFirestore();
